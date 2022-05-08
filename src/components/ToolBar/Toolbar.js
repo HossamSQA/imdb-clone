63a144b77028c5fe1a9b64d61854d764384862a5
Hossam1104 <HossamMohamed1104@gmail.com>
2022-05-09 01:32:42 +0200

    inyialize Project and finishing Toolbar

---
+++ src/components/ToolBar/Toolbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Toolbar.css";
+import HdIcon from "@mui/icons-material/Hd";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SearchIcon from "@mui/icons-material/Search";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Toolbar = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark" fixed="top">
+      <Container fluid>
+        <Navbar.Brand href="#" className=" d-flex align-items-center gap-2">
+          <HdIcon className="fs-1 text-warning" />
+          <h1 className="fw-bold mt-1">IMDB Clone</h1>
+        </Navbar.Brand>
+
+        <div className="spacer"></div>
+
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-lg-0 my-2 toolbar_items gap-3"
+            style={{ maxHeight: "50%" }}
+            navbarScroll
+          >
+            <Nav.Link href="/" className="toolbar_ico">
+              <WhatshotIcon className="fs-2 mb-1" />
+              <h6>Trending Now</h6>
+            </Nav.Link>
+
+            <Nav.Link href="/" className="toolbar_ico">
+              <MovieCreationIcon className="fs-2 mb-1" />
+              <h6>Movies</h6>
+            </Nav.Link>
+
+            <Nav.Link href="/" className="toolbar_ico">
+              <LiveTvIcon className="fs-2 mb-1" />
+              <h6>TV Series</h6>
+            </Nav.Link>
+
+            <Nav.Link href="/" className="toolbar_ico">
+              <SearchIcon className="fs-2 mb-1" />
+              <h6>Search</h6>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Toolbar;
